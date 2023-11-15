c55763751f90a85471073e14167ca7c28b464109
habinnn <gkqls7423@gmail.com>
2023-11-15 17:11:02 +0900

    springtest2

--- src/main/java/com/korea/test/TestController.java
+++ src/main/java/com/korea/test/TestController.java
@@ -24,6 +24,12 @@ public class TestController {
         //1. DB에서 데이터 꺼내오기
         List<Post> postList = postRepository.findAll();
 
+
+
+        if(postList.isEmpty()){
+            saveDefault();
+        }
+
         //2. 꺼내온 데이터를 템플릿으로 보내기
         model.addAttribute("postList", postList);
         model.addAttribute("targetPost", postList.get(0));
@@ -33,12 +39,7 @@ public class TestController {
 
     @PostMapping("/write")
     public String write() {
-        Post post = new Post();
-        post.setTitle("new title..");
-        post.setContent("");
-        post.setCreateDate(LocalDateTime.now());
-
-        postRepository.save(post);
+        saveDefault();
 
         return "redirect:/";
     }
@@ -46,6 +47,7 @@ public class TestController {
     @GetMapping("/detail/{id}")
     public String detail(Model model, @PathVariable Long id) {
         Post post = postRepository.findById(id).get();
+
         model.addAttribute("targetPost", post);
         model.addAttribute("postList", postRepository.findAll());
 
@@ -54,10 +56,28 @@ public class TestController {
     @PostMapping("/update")
     public String update(Long id, String title, String content) {
         Post post = postRepository.findById(id).get();
+
+        if(title.trim().length() == 0){
+            title = "제목없음";
+        }
         post.setTitle(title);
         post.setContent(content);
 
         postRepository.save(post);
         return "redirect:/detail/" + id;
     }
+    @PostMapping("/delete")
+    public String delete(Long id){
+        postRepository.deleteById(id);
+        return "redirect:/";
+    }
+
+    private void saveDefault(){
+        Post post = new Post();
+        post.setTitle("new title..");
+        post.setContent("");
+        post.setCreateDate(LocalDateTime.now());
+        postRepository.save(post);
+    }
+
 }
